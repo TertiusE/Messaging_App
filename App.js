3b31f7c89bb5e0317a1f5132cd672560873830ab
NATASHA AHAMMED <natasha.ahammed@outlook.com>
2022-12-06 08:12:04 -0500

    add react-native navigation packages

--- App.js
+++ App.js
@@ -1,5 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 export default function App() {
   return (
