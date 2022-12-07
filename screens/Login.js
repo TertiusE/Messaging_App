5f396775387131e74339a21071a03a5c806430a2
NATASHA AHAMMED <natasha.ahammed@outlook.com>
2022-12-07 00:41:18 -0500

    add Register styling

--- screens/Login.js
+++ screens/Login.js
@@ -14,7 +14,8 @@ import fireApp from "../config/firebase";
 import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 import { connect } from "react-redux";
 import { setUser } from "../redux/actions";
-import Logo from "../assets/logo.png";
+import Logo from "../assets/appicon.png";
+import styles from '../stylesheets/login.component'
 
 const auth = getAuth(fireApp);
 
@@ -78,72 +79,6 @@ const Login = ({ navigation, user, setUser }) => {
     </SafeAreaView>
   );
 };
-
-const styles = StyleSheet.create({
-  mainContainer: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  inputContainer: {
-    justifyContent: "stretch",
-    alignItems: "stretch",
-    width: "80%",
-  },
-  logo: {
-    width: 200,
-    height: 200,
-    alignSelf: "center",
-  },
-  header: {
-    fontSize: 24,
-    fontWeight: "700",
-    marginTop: 10,
-  },
-  subheader: {
-    fontWeight: "500",
-    fontSize: 20,
-    color: "#9F9F9F",
-    marginTop: 10,
-    marginBottom: 15,
-  },
-  inputText: {
-    backgroundColor: "#E3E7EE",
-    padding: 20,
-    borderRadius: 15,
-    marginTop: 4,
-    marginBottom: 18,
-    fontSize: 16,
-  },
-  inputLabel: {
-    justifyContent: "stretch",
-    alignSelf: "stretch",
-    color: "#A0A0A0",
-    fontWeight: "500",
-  },
-  loginButton: {
-    backgroundColor: "#5C4DF8",
-    marginTop: 20,
-    paddingTop: 20,
-    paddingBottom: 20,
-    paddingLeft: 100,
-    paddingRight: 100,
-    borderRadius: 30,
-    shadowColor: "#9F9F9F",
-    shadowOffset: {
-        width: 2,
-        height: 3
-      },
-      shadowOpacity:1
-  },
-  loginText: {
-    color: "white",
-    fontSize: 18,
-    fontWeight: "600",
-    shadowColor:'grey'
-  },
-  
-});
 
 const mapDispatch = { setUser };
 const mapState = (store) => ({
--- screens/Register.js
+++ screens/Register.js
@@ -1,47 +1,87 @@
 import { useState } from "react";
-import { StyleSheet, SafeAreaView, Text,View, TextInput, Button } from "react-native";
+import {
+  StyleSheet,
+  SafeAreaView,
+  Text,
+  View,
+  TextInput,
+  Button,
+  Image,
+  TouchableOpacity,
+} from "react-native";
 import fireApp from "../config/firebase";
 import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 import { connect } from "react-redux";
 import { setUser } from "../redux/actions";
+import Logo from "../assets/appicon.png";
+import styles from "../stylesheets/login.component";
 
-const auth = getAuth(fireApp)
+const auth = getAuth(fireApp);
 
-const Register = ({setUser, user,navigation }) => {
-    const [fName, setFName] = useState("")
-    const [lName, setLName] = useState("")
-    const [email, setEmail] = useState("")
-    const [password, setPassword] = useState("")
-    const [visibility, setVisibility] = useState(true)
+const Register = ({ setUser, user, navigation }) => {
+  const [fName, setFName] = useState("");
+  const [lName, setLName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [visibility, setVisibility] = useState(true);
 
-    const toggleVisibility = () => {
-        setVisibility(!visibility)
+  const toggleVisibility = () => {
+    setVisibility(!visibility);
+  };
+
+  const onRegister = async () => {
+    try {
+      if (email !== "" && password !== "") {
+        createUserWithEmailAndPassword(auth, email, password).then(
+          (userCredential) => {
+            setUser(userCredential.user);
+          }
+        );
+      }
+    } catch (err) {
+      console.log(err);
     }
+  };
 
-    const onRegister = async () => {
-        try{
-            if (email !== '' && password !== ''){
-                createUserWithEmailAndPassword(auth, email, password).then((userCredential) => {
-                    setUser(userCredential.user)
-                })
-            }
-        }catch (err){
-            console.log(err)
-        }
-    }
+  return (
+    <SafeAreaView style={styles.mainContainer}>
+      <Image style={styles.logo} source={Logo} />
+      <Text style={styles.header}>Register to Begin</Text>
+      <Text style={styles.subheader}>Create your account details</Text>
 
-    return(
-        <SafeAreaView>
-            <TextInput placeholder="Email" value={email} onChangeText={setEmail} />
-            <TextInput placeholder="Password" value={password} onChangeText={setPassword} />
-            <Button title="Register" onPress={onRegister}/>
-            <Button onPress={() => navigation.navigate('Login')} title="Login"/>
-        </SafeAreaView>
-    )
-}
+      <View style={styles.inputContainer}>
+        <Text style={styles.inputLabel}>Email</Text>
+        <TextInput
+          style={styles.inputText}
+          placeholder="Email"
+          value={email}
+          onChangeText={setEmail}
+        />
 
-const mapDispatch = {setUser};
+        <Text style={styles.inputLabel}>Password</Text>
+        <TextInput
+          style={styles.inputText}
+          placeholder="Password"
+          value={password}
+          onChangeText={setPassword}
+        />
+      </View>
+
+      <TouchableOpacity style={[styles.loginButton, {backgroundColor:'#919CFF'}]} onPress={onRegister}>
+        <Text style={styles.loginText}>Register</Text>
+      </TouchableOpacity>
+      <Button
+        onPress={() => navigation.navigate("Login")}
+        title="Login"
+        color="grey"
+      />
+    </SafeAreaView>
+  );
+};
+
+const mapDispatch = { setUser };
 const mapState = (store) => ({
-    user: store.dataReducer.user,
+  user: store.dataReducer.user,
 });
+
 export default connect(mapState, mapDispatch)(Register);
