745e67d816741e70b2a2ab8b9d37be24c417443c
NATASHA AHAMMED <natasha.ahammed@outlook.com>
2022-12-08 13:09:50 -0500

    add screens

--- screens/Settings.js
+++ screens/Settings.js
@@ -5,12 +5,40 @@ import Ionicons from "@expo/vector-icons/Ionicons";
 import { TouchableOpacity } from "react-native-gesture-handler";
 import Dropdown from "../components/Dropdown";
 
+const colourOptions = {
+  Blue: "#2F6EFF",
+  Steelblue: "#3D6196",
+  Skyblue: "#46C5F5",
+  Seagreen: "#4DB2BC",
+  Limegreen: "#3ACA00",
+  Olive: "#60AD52",
+  Goldenyellow: "#F5B600",
+  Orange: "#F06615",
+  Hotpink: "#F03E90",
+  Purple: "#9A6EFF",
+  Lavender: "#96A0FF",
+  Indigo: "#5C4DF8",
+};
+
+const ColorCircle = ({ color }) => {
+  return (
+    <View>
+      <View style={styles.colourSection}>
+        <TouchableOpacity
+          style={[styles.colourButton, { backgroundColor: color }]}
+        ></TouchableOpacity>
+      </View>
+    </View>
+  );
+};
+
 const Settings = () => {
   const [selected, setSelected] = useState(undefined);
   const [isEnabled, setIsEnabled] = useState(false);
-  const toggleSwitch = () => setIsEnabled(previousState => !previousState)
+  const toggleSwitch = () => setIsEnabled((previousState) => !previousState);
+  const [colorTheme, setColorTheme] = useState("Indigo");
 
-  const data = [
+  const fonts = [
     { label: "One", value: "1" },
     { label: "Two", value: "2" },
     { label: "Three", value: "3" },
@@ -26,24 +54,35 @@ const Settings = () => {
           console.log(selected.value)
         </Text>
       )} */}
-      <Dropdown style={styles.section} label={"Font"} data={data} onSelect={setSelected} />
+      <Dropdown
+        style={styles.section}
+        label={"Font"}
+        data={fonts}
+        onSelect={setSelected}
+      />
       <View style={styles.sectionContent}>
         <Text style={styles.sectionText}>Light Mode</Text>
-        <Switch 
-            trackColor={{ false: "#C1CFFF", true: "#5C4DF8" }}
-            thumbColor={isEnabled ? "white" : "#f4f3f4"}
-            ios_backgroundColor="#C1CFFF"
-            onValueChange={toggleSwitch}
-            value={isEnabled}
+        <Switch
+          trackColor={{ false: "#C1CFFF", true: "#5C4DF8" }}
+          thumbColor={isEnabled ? "white" : "#f4f3f4"}
+          ios_backgroundColor="#C1CFFF"
+          onValueChange={toggleSwitch}
+          value={isEnabled}
         />
       </View>
-      <View style={[styles.sectionContent, {borderBottomWidth:0}]}>
+      <View style={[styles.sectionContent, { borderBottomWidth: 0 }]}>
         <Text style={styles.sectionText}>Accent Colour</Text>
-        <View>
-          
-        </View>
       </View>
-
+      <View style={styles.colourSection}>
+        {Object.keys(colourOptions).map((keyName, i) => (
+          <ColorCircle color={colourOptions[keyName]} key={i}  
+          onPress={() => {
+             setColorTheme(colourOptions[keyName])
+             console.log(colorTheme)
+            }}/>
+        ))}
+      </View>
+ 
     </View>
   );
 };
@@ -51,13 +90,13 @@ const Settings = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor:'white',
+    backgroundColor: "white",
   },
   section: {
-    borderColor:"#E8E8E8",
-    borderTopWidth:1,
-    borderBottomWidth:1,
-  }, 
+    borderColor: "#E8E8E8",
+    borderTopWidth: 1,
+    borderBottomWidth: 1,
+  },
   sectionContent: {
     flexDirection: "row",
     alignItems: "center",
@@ -66,17 +105,30 @@ const styles = StyleSheet.create({
     width: "100%",
     paddingHorizontal: 20,
     zIndex: 1,
-    borderColor:"#E8E8E8",
-    borderTopWidth:1,
-    borderBottomWidth:1,
+    borderColor: "#E8E8E8",
+    borderTopWidth: 1,
+    borderBottomWidth: 1,
   },
   sectionText: {
     flex: 1,
     textAlign: "flex-start",
-    fontSize:18, 
-    fontWeight:'600', 
-    color:'#A6A6A6'
+    fontSize: 18,
+    fontWeight: "600",
+    color: "#A6A6A6",
+  },
+  colourSection: {
+    alignItems: "center",
+    justifyContent: "center",
+    marginHorizontal: 4,
+    marginVertical: 4,
+    flexWrap: true,
+    flexDirection: "row",
+    flexWrap: "wrap",
+  },
+  colourButton: {
+    height: 80,
+    width: 80,
+    borderRadius: 40,
   },
 });
-
 export default Settings;
