d216da28c698ad6694dd5b442863282c3279869e
NATASHA AHAMMED <natasha.ahammed@outlook.com>
2022-12-08 23:10:51 -0500

    add profile, settings and fonts for testing

--- screens/Profile.js
+++ screens/Profile.js
@@ -1,16 +1,295 @@
-import React from 'react'
-import { Text } from 'react-native'
-const Profile = ({ user, accentColour, systemFont }) => {
-  return (
-    <Text>Profile</Text>
-  )
-}
+import React from "react";
+import { useState } from "react";
+import {
+  SafeAreaView,
+  Text,
+  StyleSheet,
+  Image,
+  Button,
+  View,
+  TextInput,
+  KeyboardAvoidingView,
+  ScrollView,
+  TouchableOpacity,
+  Modal,
+  Pressable,
+} from "react-native";
+import ProfileImg from "../assets/profile-icon.png";
 
-const mapDispatch = { setUser, setAccentColour, setFont };
-const mapState = (store) => ({
-  user: store.dataReducer.user,
-  accentColour: store.dataReducer.accentColour,
-  systemFont: store.dataReducer.systemFont
+const Profile = () => {
+  const [fName, setfName] = useState("");
+  const [lName, setlName] = useState("");
+  const [bDay, setbDay] = useState("");
+  const [bMonth, setbMonth] = useState("");
+  const [bYear, setbYear] = useState("");
+  const [birthDate, setBirthDate] = useState(new Date());
+  const [profileImg, setProfileImg] = useState("");
+  const [modalVisible, setModalVisible] = useState(false);
+
+  /* Dispatch User Info changes */
+  const onSaveChangesClicked = () => {
+    let newBirthDate = new Date(`${bMonth} ${bDay}, ${bYear}`);
+    setBirthDate(newBirthDate);
+    let userChanges = {};
+
+    // Validate user input before storing
+    if (fName) {
+      userChanges.firstName = fName;
+    }
+    if (lName) {
+      userChanges.lastName = fName;
+    }
+    if (bDay && bMonth && bYear && !Number.isNaN(Date.parse(newBirthDate))) {
+      userChanges.birthDate = birthDate;
+    }
+    if (/\.(jpg|jpeg|png|webp|avif|gif|svg)$/.test(profileImg)) {
+      userChanges.profileImage = profileImg;
+      setProfileImg(profileImg)
+    } else {
+      setProfileImg(
+        "https://s3-eu-west-1.amazonaws.com/artsthread-content/images/users/68ebb7a3c21864ae50b17a28b4866a94.jpg"
+      );
+    }
+
+    console.log(userChanges);
+
+    setfName("");
+    setlName("");
+    setbDay("");
+    setbMonth("");
+    setbYear("");
+  };
+
+  return (
+    <SafeAreaView style={styles.mainContainer}>
+      {/* Modal View */}
+      <Modal
+        animationType="fade"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={setModalVisible}
+      >
+        <View style={styles.centeredView}>
+          <View style={styles.modalView}>
+            <Text style={styles.modalText}>Change Image</Text>
+            <TextInput
+              style={styles.inputText}
+              placeholder="Image URL"
+              value={profileImg}
+              onChangeText={setProfileImg}
+            />
+
+            <TouchableOpacity
+              style={[styles.button, styles.buttonClose]}
+              onPress={() => setModalVisible(!modalVisible)}
+            >
+              <Text style={{color:'white'}}>Close</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </Modal>
+
+      <Image
+        style={styles.image}
+        source={
+          profileImg
+            ? { uri: `${profileImg}` }
+            : {
+                uri: "https://s3-eu-west-1.amazonaws.com/artsthread-content/images/users/68ebb7a3c21864ae50b17a28b4866a94.jpg",
+              }
+        }
+      />
+
+      {fName !== "" && lName !== "" ? (
+        <Text style={styles.header}>
+          {fName} {lName}
+        </Text>
+      ) : (
+        <Text style={styles.header}>FirstName LastName</Text>
+      )}
+
+      <Button
+        onPress={() => setModalVisible(true)}
+        title="Change photo"
+        color="#A5ADF9"
+      ></Button>
+
+      <View style={styles.inputContainer}>
+        <Text style={styles.inputLabel}>First Name</Text>
+        <TextInput
+          style={styles.inputText}
+          placeholder="First Name"
+          value={fName}
+          onChangeText={setfName}
+        />
+
+        <Text style={styles.inputLabel}>Last Name</Text>
+        <TextInput
+          style={styles.inputText}
+          placeholder="Last Name"
+          value={lName}
+          onChangeText={setlName}
+        />
+
+        <Text style={styles.inputLabel}>Date of Birth</Text>
+        <View style={styles.bDateContainer}>
+          <View style={styles.bDateItem}>
+            <TextInput
+              style={[styles.inputText, styles.inputBDate]}
+              placeholder="DD"
+              value={bDay}
+              onChangeText={setbDay}
+            />
+            <TextInput
+              style={[styles.inputText, styles.inputBDate]}
+              placeholder="MMM"
+              value={bMonth}
+              onChangeText={setbMonth}
+            />
+            <TextInput
+              style={[styles.inputText, styles.inputBDate]}
+              placeholder="YYYY"
+              value={bYear}
+              onChangeText={setbYear}
+            />
+          </View>
+        </View>
+        <TouchableOpacity
+          style={styles.saveButton}
+          onPress={() => onSaveChangesClicked()}
+        >
+          <Text style={styles.saveText}>Save Changes</Text>
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  mainContainer: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "white",
+  },
+  image: {
+    width: 150,
+    height: 150,
+    borderRadius: 100,
+  },
+  header: {
+    fontSize: 24,
+    fontWeight: "700",
+    marginTop: 10,
+  },
+  inputContainer: {
+    justifyContent: "stretch",
+    alignItems: "stretch",
+    width: "80%",
+    flex: 0.8,
+  },
+  inputLabel: {
+    color: "#999999",
+    fontSize: 16,
+    fontWeight: "600",
+    marginBottom: 5,
+    marginTop: 25,
+  },
+  inputText: {
+    borderBottomColor: "#D7D7D7",
+    borderBottomWidth: 1,
+    color: "black",
+    paddingTop: 5,
+    paddingBottom: 5,
+    fontSize: 16,
+    fontWeight: "500",
+  },
+  bDateContainer: {
+    flex: 1,
+    alignItems: "stretch",
+    justifyContent: "stretch",
+  },
+  bDateItem: {
+    height: 50,
+    flexDirection: "row",
+    alignSelf: "stretch",
+    justifySelf: "stretch",
+  },
+  inputBDate: {
+    alignSelf: "center",
+    textAlign: "center",
+    flex: 1,
+    margin: 5,
+  },
+  saveButton: {
+    width: "90%",
+    alignSelf: "center",
+    backgroundColor: "#5C4DF8",
+    marginTop: 20,
+    paddingTop: 15,
+    paddingBottom: 15,
+    paddingLeft: 80,
+    paddingRight: 80,
+    borderRadius: 20,
+    shadowColor: "#9F9F9F",
+    shadowOffset: {
+      width: 2,
+      height: 3,
+    },
+    shadowOpacity: 1,
+  },
+  saveText: {
+    color: "white",
+    fontSize: 18,
+    fontWeight: "600",
+    shadowColor: "grey",
+  },
+  centeredView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 22,
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: "white",
+    borderRadius: 20,
+    padding: 35,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.10,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  button: {
+    borderRadius: 30,
+    padding:15,
+    paddingLeft: 25,
+    paddingRight: 25,
+    elevation: 2,
+    color:'white',
+  },
+  buttonClose: {
+    marginTop:15,
+    backgroundColor: "#A5ADF9",
+    color:'white',
+    fontSize:20,
+  },
+  textStyle: {
+    color: "white",
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: "center",
+    fontSize:20,
+    fontWeight:'700'
+  },
 });
 
-export default connect(mapState, mapDispatch)(Profile);
+export default Profile;
--- screens/Settings.js
+++ screens/Settings.js
@@ -1,16 +1,170 @@
-import React from 'react'
-import { Text } from 'react-native'
-const Settings = ({ user, accentColour, systemFont }) => {
+import { React, useState, useRef } from "react";
+import { Text, View, StyleSheet, Modal, FlatList, Switch } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import Ionicons from "@expo/vector-icons/Ionicons";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import { Picker } from "@react-native-picker/picker";
+import * as SplashScreen from "expo-splash-screen";
+
+import { useFonts } from "expo-font";
+import { useCallback } from "react";
+
+const colourOptions = {
+  Blue: "#2F6EFF",
+  Steelblue: "#3D6196",
+  Skyblue: "#46C5F5",
+  Seagreen: "#4DB2BC",
+  Limegreen: "#3ACA00",
+  Olive: "#60AD52",
+  Goldenyellow: "#F5B600",
+  Orange: "#F06615",
+  Hotpink: "#F03E90",
+  Purple: "#9A6EFF",
+  Lavender: "#96A0FF",
+  Indigo: "#5C4DF8",
+};
+
+const ColorCircle = ({ color, handleClick, colorThemeSelected }) => {
   return (
-    <Text>Settings</Text>
-  )
-}
+    <View>
+      <View style={styles.colourSection}>
+        <TouchableOpacity
+          style={[styles.colourButton, { backgroundColor: color }]}
+          onPress={() => handleClick(color)}
+        >
+          {colorThemeSelected == color && (
+            <Ionicons
+              style={styles.checkmark}
+              name="checkmark"
+              size={50}
+              color="white"
+            />
+          )}
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
 
-const mapDispatch = { setUser, setAccentColour, setFont };
-const mapState = (store) => ({
-  user: store.dataReducer.user,
-  accentColour: store.dataReducer.accentColour,
-  systemFont: store.dataReducer.systemFont
+SplashScreen.preventAutoHideAsync();
+
+const Settings = () => {
+  const [isEnabled, setIsEnabled] = useState(false);
+  const toggleSwitch = () => setIsEnabled((previousState) => !previousState);
+  const [colorTheme, setColorTheme] = useState("#5C4DF8");
+  const [font, setFont] = useState("sans-serif");
+
+  const handleColorClick = (colorPressed) => {
+    setColorTheme(colorPressed);
+  };
+
+  const [fontsLoaded] = useFonts({
+    Caveat: require("../assets/fonts/Caveat/Caveat-Bold.ttf"),
+    Roboto: require("../assets/fonts/Roboto/Roboto-Regular.ttf"),
+    RobotoSlab: require("../assets/fonts/Roboto_Slab/RobotoSlab-Medium.ttf"),
+    Poppins: require("../assets/fonts/Poppins/Poppins-Medium.ttf"),
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
+  return (
+    <View style={styles.container} onLayout={onLayoutRootView}>
+      <View style={styles.sectionContent}>
+        <Text style={styles.sectionText}>Font</Text>
+      </View>
+      <View style={styles.sectionContent}>
+        <Text style={styles.sectionText}>
+          {isEnabled ? "Dark Mode" : "Light Mode"}
+        </Text>
+        <Switch
+          trackColor={{ false: "#C1CFFF", true: "#5C4DF8" }}
+          thumbColor={isEnabled ? "white" : "#f4f3f4"}
+          ios_backgroundColor="#C1CFFF"
+          onValueChange={toggleSwitch}
+          value={isEnabled}
+        />
+      </View>
+      <View style={[styles.sectionContent, { borderBottomWidth: 0 }]}>
+        <Text style={[styles.sectionText]}>Accent Colour</Text>
+      </View>
+      <View style={styles.colourSection}>
+        {Object.keys(colourOptions).map((keyName, i) => (
+          <ColorCircle
+            color={colourOptions[keyName]}
+            key={i}
+            handleClick={handleColorClick}
+            colorThemeSelected={colorTheme}
+          />
+        ))}
+      </View>
+      {/* <Picker
+        selectedValue={font}
+        onValueChange={(value) => {
+          setFont(value);
+        }}
+      >
+        <Picker.Item label="Caveat" value="Caveat" />
+        <Picker.Item label="Roboto" value="Roboto" />
+      </Picker>
+      <View>
+        <Text>{font}</Text>
+      </View> */}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "white",
+  },
+  section: {
+    borderColor: "#E8E8E8",
+    borderTopWidth: 1,
+    borderBottomWidth: 1,
+  },
+  sectionContent: {
+    flexDirection: "row",
+    alignItems: "center",
+    backgroundColor: "white",
+    height: 50,
+    width: "100%",
+    paddingHorizontal: 20,
+    zIndex: 1,
+    borderColor: "#E8E8E8",
+    borderTopWidth: 1,
+    borderBottomWidth: 1,
+  },
+  sectionText: {
+    flex: 1,
+    textAlign: "flex-start",
+    fontSize: 18,
+    fontWeight: "600",
+    color: "#A6A6A6",
+  },
+  colourSection: {
+    alignItems: "center",
+    justifyContent: "center",
+    marginHorizontal: 4,
+    marginVertical: 4,
+    flexWrap: true,
+    flexDirection: "row",
+    flexWrap: "wrap",
+  },
+  colourButton: {
+    height: 80,
+    width: 80,
+    borderRadius: 40,
+    justifyContent: "center",
+    alignItems: "center",
+  },
 });
-
-export default connect(mapState, mapDispatch)(Settings);
+export default Settings;
