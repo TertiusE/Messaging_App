bd06365461dab9d667f892b75ac0d98d6ec56b2c
NATASHA AHAMMED <natasha.ahammed@outlook.com>
2022-12-09 19:41:42 -0500

    complete loading chat messages feature

--- screens/Message.js
+++ screens/Message.js
@@ -1,39 +1,91 @@
 import { React, useState, useEffect, useCallback } from "react";
-import { Text, View, StyleSheet, TextInput, ScrollView, KeyboardAvoidingView, Button, Platform } from "react-native";
-import { onSnapshot, doc, getFirestore, updateDoc, arrayUnion } from "firebase/firestore"
-import { setAccentColour, setLoading, setUser, setSystemFont } from "../redux/actions";
+import {
+  Text,
+  View,
+  StyleSheet,
+  TextInput,
+  ScrollView,
+  KeyboardAvoidingView,
+  Button,
+  Platform,
+} from "react-native";
+import {
+  onSnapshot,
+  doc,
+  getFirestore,
+  updateDoc,
+  arrayUnion,
+} from "firebase/firestore";
+import {
+  setAccentColour,
+  setLoading,
+  setUser,
+  setSystemFont,
+} from "../redux/actions";
 import { connect } from "react-redux";
-import uuid from 'react-native-uuid';
+import uuid from "react-native-uuid";
 import fireApp from "../config/firebase";
-import { GiftedChat } from 'react-native-gifted-chat';
+import { GiftedChat } from "react-native-gifted-chat";
 
-const db = getFirestore(fireApp)
+const db = getFirestore(fireApp);
 
 const Message = ({ user, accentColour, systemFont, route }) => {
   const [text, setText] = useState("");
-  const { otherUser } = route.params
-  const [messages, setMessages] = useState([])
+  const { otherUser } = route.params;
+  const [messages, setMessages] = useState([]);
+  const [messageArray, setMessageArray] = useState([]);
 
   useEffect(() => {
     const fetchData = async () => {
-      let allMessages = []
+      let allMessages = [];
       const userRef = onSnapshot(doc(db, "conversations", user.uid), (doc) => {
-        doc.data().messages.forEach(element => {
-          if ((element.sent_to == user.uid && element.sent_by == otherUser.uid) || (element.sent_to == otherUser.uid && element.sent_by == user.uid)) {
-            allMessages.push(element)
+        doc.data().messages.forEach((element) => {
+          if (
+            (element.sent_to == user.uid && element.sent_by == otherUser.uid) ||
+            (element.sent_to == otherUser.uid && element.sent_by == user.uid)
+          ) {
+            allMessages.push(element);
           }
-        })
-        setMessages(allMessages)
+        });
+        setMessages(allMessages);
       });
-    }
-    fetchData()
-      .catch(console.error);
-  }, [])
+    };
+    fetchData().catch(console.error);
+
+  }, []);
+
+  useEffect(() => {
+    const promise = new Promise((resolve, reject) => {
+      let allMessages = [];
+      resolve(messages.forEach((object) => {
+        let result = {
+          _id: object.sent_to,
+          text: object.text,
+          createdAt: object.sent_at,
+          user: {
+            _id: object.sent_by,
+            name: object.id,
+            avatar: "https://placeimg.com/140/140/any",
+          },
+        }
+
+        allMessages.push(result)
+        console.log(object)
+        setMessageArray(allMessages)
+      }))
+      reject(
+        console.log("Error retrieving messages")
+      )
+    });
+    promise.then((value) => {
+      console.log(value)
+    }, [])
+  }, [messages]);
 
   let sendMessage = async () => {
-    console.log(user.uid)
-    console.log(otherUser.uid)
-    const current_time = new Date()
+    console.log(user.uid);
+    console.log(otherUser.uid);
+    const current_time = new Date();
     const userRef = doc(db, "conversations", user.uid);
     const userUnion = await updateDoc(userRef, {
       messages: arrayUnion({
@@ -41,8 +93,8 @@ const Message = ({ user, accentColour, systemFont, route }) => {
         sent_by: user.uid,
         sent_to: otherUser.uid,
         sent_at: current_time,
-        text: text
-      })
+        text: text,
+      }),
     });
 
     const otherRef = doc(db, "conversations", otherUser.uid);
@@ -52,49 +104,52 @@ const Message = ({ user, accentColour, systemFont, route }) => {
         sent_by: user.uid,
         sent_to: otherUser.uid,
         sent_at: current_time,
-        text: text
-      })
+        text: text,
+      }),
     });
-    setText("")
-  }
+    setText("");
+  };
 
   const [testMessages, setTestMessages] = useState([]);
 
   useEffect(() => {
     setTestMessages([
       {
-        _id: 1, // sender 
-        sent_by: '',
+        _id: 1, // sender
+        sent_by: "",
         createdAt: new Date(),
-        text:'hello',
+        text: "hello",
         user: {
-          _id: 'QT3zmmAuw7ZYbURLCzUqTtEYah73', // reciever
-          name: 'React Native',
-          avatar: 'https://placeimg.com/140/140/any',
+          _id: "QT3zmmAuw7ZYbURLCzUqTtEYah73", // reciever
+          name: "React Native",
+          avatar: "https://placeimg.com/140/140/any",
         },
       },
-    ])
-  }, [])
-  
+    ]);
+  }, []);
+
   const onSend = useCallback((testmessage = []) => {
-    sendMessage()
-    setTestMessages(previousMessages => GiftedChat.append(previousMessages, testmessage))
-    console.log(testmessage)
-  })
+    sendMessage();
+    setMessageArray((previousMessages) =>
+      GiftedChat.append(previousMessages, testmessage)
+    );
+    console.log(testmessage);
+  });
 
   function handMessageClick(textVal) {
-    setText(textVal)
+    setText(textVal);
   }
 
-
   return (
-    <GiftedChat 
-      messages={testMessages}
+    <GiftedChat
+      messages={messageArray == undefined ? console.log("no items") : messageArray}
       onInputTextChanged={(text) => handMessageClick(text)}
       onSend={(message) => onSend(message)}
       user={{
-        _id:1,
-      }}
+        _id: user.uid,
+        name: user.fName,
+        avatar: user.photoUrl
+    }}
     />
   );
 };
@@ -106,7 +161,7 @@ const styles = StyleSheet.create({
   },
   messagesSection: {
     flex: 1,
-    backgroundColor: '#F9F9F9'
+    backgroundColor: "#F9F9F9",
   },
   inputSection: {
     flex: 0.5,
@@ -126,13 +181,12 @@ const styles = StyleSheet.create({
   },
 });
 
-
 const mapDispatch = { setUser, setAccentColour, setSystemFont, setLoading };
 const mapState = (store) => ({
-    user: store.dataReducer.user,
-    accentColour: store.dataReducer.accentColour,
-    systemFont: store.dataReducer.systemFont,
-    isLoading: store.dataReducer.isLoading
+  user: store.dataReducer.user,
+  accentColour: store.dataReducer.accentColour,
+  systemFont: store.dataReducer.systemFont,
+  isLoading: store.dataReducer.isLoading,
 });
 
 export default connect(mapState, mapDispatch)(Message);
