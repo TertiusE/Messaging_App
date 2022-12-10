f20c5f50c797c547a986f38430794f3876379c2a
Tertius Erskine <terersk@gmail.com>
2022-12-09 19:29:50 -0500

    Fixed navigation warning and fixed async storage warning in app

--- App.js
+++ App.js
@@ -2,8 +2,10 @@ import { StatusBar } from 'expo-status-bar';
 import { Provider } from 'react-redux';
 import store from './redux/store';
 import HandleNav from "./navigation/HandleNav";
+import { LogBox } from 'react-native';
 
 export default function App() {
+  LogBox.ignoreLogs(['AsyncStorage has been extracted from react-native core and will be removed in a future release.']);
   return (
     <Provider store={store}>
       <HandleNav />
